eda55866661990d5fe5694636ae59c1eb724ee67
go_coldcat <57941767+gcc-bug@users.noreply.github.com>
2023-10-16 16:51:28 +0800

    fixed c++ 17

--- .history/basic/main_20231013171425.cpp
+++ .history/basic/main_20231013171425.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <Config.h>
 #include <ctime>
-#include <variant>
 int main(int argc, char* argv[])
 {
      if (argc < 2) {
---
+++ basic/build/Config.h
@@ -0,0 +1,6 @@
+// the configured options and settings for main
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 2
+
+#define TIMESTAMP 20232916-162909
